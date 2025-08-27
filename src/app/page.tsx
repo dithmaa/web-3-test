ccdd5a52e4a76f1901437cbcedce160904d5d1bb
Nomto <disma785@gmail.com>
2025-08-27 20:23:09 +0900

    feat: add stitches types, stitches styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,38 +4,43 @@ import { ConnectButton, GetBalanceButton } from "@/entities/wallet";
 import { InfoList } from "@/components/InfoList";
 import { ActionButtonList } from "@/components/ActionButtonList";
 import { styled } from "@/shared/lib";
-import type * as Stitches from '@stitches/react';
 
-const BaseDiv = styled('div', {});
-
-export const FlexBetween = styled(BaseDiv, {
+export const FlexBetween = styled("div", {
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
 });
 
-export const Flex = styled(BaseDiv, {
+export const Flex = styled("div", {
   display: "flex",
   alignItems: "center",
+});
+
+export const Container = styled("div", {
+  maxWidth: "1170px",
+  width: "90%",
+  margin: "0 auto",
 });
 
 export default function Home() {
   return (
     <div className={"pages"}>
-      <header>
-        <nav>
-          <FlexBetween>
-            <a href="#">Web3 Logo</a>
-            <Flex>
-              <ConnectButton />
-              <GetBalanceButton />
-            </Flex>
-          </FlexBetween>
-        </nav>
-      </header>
+      <Container>
+        <header>
+          <nav>
+            <FlexBetween>
+              <a href="#">Web3 Logo</a>
+              <Flex>
+                <ConnectButton />
+                <GetBalanceButton />
+              </Flex>
+            </FlexBetween>
+          </nav>
+        </header>
 
-      <ActionButtonList />
-      <InfoList />
+        <ActionButtonList />
+        <InfoList />
+      </Container>
     </div>
   );
 }
--- src/shared/lib/stitches.ts
+++ src/shared/lib/stitches.ts
@@ -1,20 +1,24 @@
 import { createStitches } from '@stitches/react';
+import type * as Stitches from "@stitches/react";
 
-export const { styled, css, theme } = createStitches({
+export const stitchesConfig = createStitches({
   theme: {
     colors: {
-      primary: '#4f46e5',
-      primaryHover: '#4338ca',
-      white: '#ffffff',
+      primary: "#4f46e5",
+      primaryHover: "#4338ca",
+      white: "#ffffff",
     },
     space: {
-      sm: '8px',
-      md: '16px',
-      lg: '24px',
+      sm: "8px",
+      md: "16px",
+      lg: "24px",
     },
     radii: {
-      sm: '4px',
-      md: '8px',
+      sm: "4px",
+      md: "8px",
     },
   },
 });
+
+export type CSS = Stitches.CSS<(typeof stitchesConfig)["config"]>;
+export const { styled, css, config, theme } = stitchesConfig;
